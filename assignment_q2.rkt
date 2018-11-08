7b570d8b79b009a78b6df21651fbf0f08dd54669
NoelMinogue <noelminogue97@gmail.com>
2018-11-08 19:58:32 +0000

    Update assignment_q2.rkt

--- assignment_q2.rkt
+++ assignment_q2.rkt
@@ -1,17 +1,51 @@
 #lang racket
 
-;This is an example implementation of ins_beg,
-;It obviously doesn't do what it should, so you
-;can edit this function to get started.
-;
-;Please note the provide function is necessary
-;for the unit tests to work. Please include a
-;(provide) for each function you write in your
-;submitted assignment.
-;
-;You may delete these comments!
 
 (provide ins_beg)
 
 (define (ins_beg el lst)
-  (display "Hello, I'm ins_beg!\n"))
+  
+  (cons el lst)
+  
+)
+
+(provide ins_end)
+
+(define (ins_end el lst)
+  
+ (append  lst (cons el empty))
+  
+)
+
+(provide cout_top_level)
+
+(define (cout_top_level list)
+  (cond [(null? list) 0]
+      
+      [else (+ 1 (cout_top_level (cdr list)))]
+  )
+)
+
+;Could not do part D
+
+
+
+
+(provide count_instances_tr)
+
+(define (count_instances_tr el list)
+  
+   (cond ((null? list)0) ;while its not null
+         
+   ((= el (car list))(+ 1(count_instances_tr el (cdr list)))) 
+      ;if the element is at the head of the list, we increment the number
+   ;otherwise we call the fucntion again for the rest of the list.
+    ((count_instances_tr el (cdr list)))
+    
+   )
+  
+  
+  )
+(count_instances_tr 2 '(1 2 2 2 3 2 4))
+
+;Could not do part F
