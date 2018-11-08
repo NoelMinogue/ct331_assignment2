5b1cf3792ccecb7ef1d4e795a0f1dd7a8f9d423e
NoelMinogue <noelminogue97@gmail.com>
2018-11-08 19:56:53 +0000

    Update assignment_q1.rkt

--- assignment_q1.rkt
+++ assignment_q1.rkt
@@ -1,1 +1,11 @@
 #lang racket
+;Part 1
+(cons 3 4)
+;Part 2
+(cons (cons 1 (cons 2 (cons 3 empty))) empty)
+;Part 3
+(cons "string" (cons 7 (cons (cons 1 ( cons 2 (cons 3 empty))) empty)))
+;Part 4
+(list "string" 7 (list 1 2 3))
+;Part 5
+(append '(string) '(7) (append '(1) '(2) '(3)))
